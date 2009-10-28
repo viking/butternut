a2b31cb83393e0c5b0aa1d07f84640a5b16b71c2
Jeremy Stephens <viking415@gmail.com>
2009-10-28 13:17:29 -0500

    Starting from Cucumber's HTML formatter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,68 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'butternut'
+require 'rubygems'
+gem 'rspec'
 require 'spec'
 require 'spec/autorun'
 
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'butternut'
+
+require 'cucumber/formatter/html'
+require 'cucumber/rb_support/rb_language'
+require 'nokogiri'
+
+module Cucumber
+  module Formatter
+    module SpecHelperDsl
+      attr_reader :feature_content, :step_defs
+
+      def define_feature(string)
+        @feature_content = string
+      end
+
+      def define_steps(&block)
+        @step_defs = block
+      end
+    end
+
+    module SpecHelper
+      def run_defined_feature
+        define_steps
+        features = load_features(self.class.feature_content || raise("No feature content defined!"))
+        run(features)
+      end
+
+      def step_mother
+        @step_mother ||= StepMother.new
+      end
+
+      def load_features(content)
+        feature_file = FeatureFile.new('spec.feature', content)
+        features = Ast::Features.new
+        features.add_feature feature_file.parse(@step_mother, {})
+        features
+      end
+
+      def run(features)
+        # options = { :verbose => true }
+        options = {}
+        tree_walker = Cucumber::Ast::TreeWalker.new(@step_mother, [@formatter], options, STDOUT)
+        tree_walker.visit_features(features)
+      end
+
+      def define_steps
+        return unless step_defs = self.class.step_defs
+        rb = @step_mother.load_programming_language('rb')
+        dsl = Object.new
+        dsl.extend RbSupport::RbDsl
+        dsl.instance_exec &step_defs
+      end
+    end
+  end
+end
+
 Spec::Runner.configure do |config|
-  
+  config.before(:each) do
+    Cucumber::Parser::NaturalLanguage.instance_variable_set(:@languages, nil)
+  end
 end
